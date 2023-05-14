5c2d9db26d4591328ce90c80ff57a92782fc1b35
Joao <joaogabriel443@gmail.com>
2023-05-13 22:40:23 -0300

    application.yml e anotações do JPA

--- src/main/kotlin/dev/joao/request/credit/system/entity/Credit.kt
+++ src/main/kotlin/dev/joao/request/credit/system/entity/Credit.kt
@@ -1,16 +1,26 @@
 package dev.joao.request.credit.system.entity
 
 import dev.joao.request.credit.system.enums.Status
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.UUID
 
+@Entity
+@Table(name = "credit")
 data class Credit(
-        val creditCode: UUID = UUID.randomUUID(),
-        val creditValue: BigDecimal = BigDecimal.ZERO,
-        val dayFirstInstallment: LocalDate,
-        val numberOfInstallment: Int = 0,
-        var status: Status = Status.IN_PROGRESS,
-        val customer: Customer? = null,
-        var id: Long? = null
+        @Column(nullable = false, unique = true) val creditCode: UUID = UUID.randomUUID(),
+        @Column(nullable = false) val creditValue: BigDecimal = BigDecimal.ZERO,
+        @Column(nullable = false) val dayFirstInstallment: LocalDate,
+        @Column(nullable = false) val numberOfInstallment: Int = 0,
+        @Enumerated var status: Status = Status.IN_PROGRESS,
+        @ManyToOne val customer: Customer? = null,
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null
 )
--- src/main/kotlin/dev/joao/request/credit/system/entity/Customer.kt
+++ src/main/kotlin/dev/joao/request/credit/system/entity/Customer.kt
@@ -1,12 +1,24 @@
 package dev.joao.request.credit.system.entity
 
+import jakarta.persistence.*
+import org.springframework.lang.Nullable
+
+@Entity
+@Table(name = "Customer")
 data class Customer(
-        var firstName: String = "",
-        var lastName:String = "",
-        val cpf: String,
-        var email: String = "",
-        var password: String = "",
-        var address: Address = Address(),
+        @Column(nullable = false) var firstName: String = "",
+        @Column(nullable = false) var lastName: String = "",
+        @Column(nullable = false, unique = true) val cpf: String,
+        @Column(nullable = false, unique = true) var email: String = "",
+        @Column(nullable = false) var password: String = "",
+        @Column(nullable = false) @Embedded var address: Address = Address(),
+        @Column(nullable = false) @OneToMany(fetch = FetchType.LAZY,
+                cascade = arrayOf(CascadeType.REMOVE),
+                mappedBy = "customer")
         var credits: List<Credit> = mutableListOf(),
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         var id: Long? = null,
-)
+){
+
+}
